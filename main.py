52e5e98d560ab750e24b4b80ce7c6a497aceb5bc
IgorKopitsa <igor.kopitsa68@yandex.ru>
2024-07-03 19:48:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ print("1st program")
 print(9 ** 0.5*5)
 print("2nd program")
 print(9.99>9.98 and 1000 != 1000.1) # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+print("3rd program")
+a= 1234
+middle = a % 1000 // 10
+print(middle)
+a=5678
+middle = a % 5000 // 10
+print(middle)
+a = 23
+b = 67
+print(a+b)
+print("4th program")
+a= 13.42
+b= 42.13
+print(42==42 "13.42 == 42.13")
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
